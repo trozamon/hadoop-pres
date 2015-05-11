4ccb701dcecab7afb43f0620fbb9b8ac46d38439
Alec Ten Harmsel <alec@alectenharmsel.com>
2015-05-11 14:09:34 -0400

    Fix python script

--- warez/python.sh
+++ warez/python.sh
@@ -2,8 +2,11 @@
 yarn jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -Dmapreduce.job.queuename=training \
     -input ngrams.data -output ngrams-output \
-    -mapper warez/map.py -file warez/map.py \
-    -reducer warez/reduce.py -file warez/reduce.py
+    -mapper warez/map.py -reducer warez/reduce.py \
+    -file warez/map.py -file warez/reduce.py
+
+# Look at the output from the job
+hdfs dfs -cat ngrams-output/part*
 
 # Run locally for testing
 cat ngrams.data | python3.3 warez/map.py | python3.3 warez/reduce.py
